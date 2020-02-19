ec70a379bcd77bdb22760ae2925a8507ed7f21f9
LambdaGaming <starwars18m@gmail.com>
2020-02-18 22:55:16 -0500

    Started to add main app

---
+++ Main/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GMod_Server_Launcher_Console
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault( false );
+			Application.Run( new MainForm() );
+			Properties.Settings.Default.Save();
+		}
+	}
+}
