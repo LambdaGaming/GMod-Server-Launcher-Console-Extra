179868378485aec76a750b922f2d72d6e368c056
LambdaGaming <starwars18m@gmail.com>
2020-05-04 17:52:06 -0400

    Removed console version of CityRP launcher
    Also added token support to sandbox version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Garry's Mod Server Launcher GUI With Console Support - Extra Builds
- These are extra builds of the Gmod server launcher that I use for my own servers. I'm including them here since they're slightly different from the main build and some people may want to edit them for their own needs. Please note however that I will not be giving support for these builds, and that, unlike the main build, most features of these builds are hard-coded and cannot be changed without a recompile.
+ These are extra builds of the Gmod server launcher that I use for my own servers. I'm including them here since they're slightly different from the main build and some people may want to edit them for their own needs. Please note however that I will not be giving support for these builds, and unlike the main build, the configs of these builds are hard-coded and cannot be changed without a recompile.
 
 ## Variations:
  Below are the included variations of the server launcher and their features.
@@ -9,7 +9,6 @@
 	<li>LAN mode toggle</li>
 	<li>Map selection</li>
 	<li>Max players selection</li>
-	<li>Separate console version with only a map selection</li>
 </ul>
 
 ### Half-Life Universe RP:
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -3,81 +3,79 @@ using System.Diagnostics;
 
 namespace ServerLauncher
 {
-    class Program
-    {
-        static void Main()
-        {
-            // Config, might move to a separate file at some point so it doesn't need recompiled
-            string Dir = @"E:\gmodserver"; // File path of the server exe, don't include the actual file
-            string[] Maps = new[] { // List of map names to get randomly chosen
+	class Program
+	{
+		static void Main()
+		{
+			// Config starts here
+			string Dir = @"E:\gmodserver"; // File path of the server exe, don't include the actual file
+			string[] Maps = { // List of map names to get randomly chosen
                 "fightspace3b",
-                "gm_balkans",
-                "gm_bigcity",
-                "gm_bigisland",
-                "gm_blackmesa_sigma",
-                "gm_bluehills_test3",
-                "gm_boreas",
-                "gm_botmap_v3",
-                "gm_construct",
-                "gm_construct_flagrass_v6-2",
-                "gm_dddustbowl2",
-                "gm_drivingmap_workshop",
-                "gm_flatgrass",
-                "gm_flatgrass_abs_v3c",
-                "gm_fork",
-                "gm_freespace_09_extended",
-                "gm_functional_flatgrass3",
-                "gm_genesis",
-                "gm_highway14800",
-                "gm_lunarbase",
-                "gm_mobenix_v3_final",
-                "gm_mountainlake2",
-                "gm_range_f4",
-                "gm_secretconstruct_v3",
-                "gm_tornadoflatgrass",
-                "gm_tornadohighway_final",
-                "gm_tornadonightfall",
-                "gm_tornadovillage_final",
-                "gm_underground_iv",
-                "gm_valley",
-                "gm_warmap_v5",
-                "rp_rockford_open",
-                "gm_atomic",
-                "rp_1944berlin_v1",
-                "rp_bmrf",
-                "rp_city17_build210",
-                "rp_downtown_night_v2",
-                "rp_ineu_valley2_v1a",
-                "rp_sectorc_beta",
-                "rp_stalingrad_gamizer",
-                "rp_unioncity_day"
-            };
-            string Gamemode = "sandbox"; // Name of the gamemode folder
-            int MaxPlayers = 24; // Number of players allowed on the server
-            // End Config
+				"gm_balkans",
+				"gm_bigcity",
+				"gm_bigisland",
+				"gm_blackmesa_sigma",
+				"gm_bluehills_test3",
+				"gm_boreas",
+				"gm_botmap_v3",
+				"gm_construct",
+				"gm_construct_flagrass_v6-2",
+				"gm_dddustbowl2",
+				"gm_drivingmap_workshop",
+				"gm_flatgrass",
+				"gm_flatgrass_abs_v3c",
+				"gm_fork",
+				"gm_freespace_09_extended",
+				"gm_functional_flatgrass3",
+				"gm_genesis",
+				"gm_highway14800",
+				"gm_lunarbase",
+				"gm_mobenix_v3_final",
+				"gm_mountainlake2",
+				"gm_range_f4",
+				"gm_secretconstruct_v3",
+				"gm_tornadoflatgrass",
+				"gm_tornadohighway_final",
+				"gm_tornadonightfall",
+				"gm_tornadovillage_final",
+				"gm_underground_iv",
+				"gm_valley",
+				"gm_warmap_v5",
+				"rp_rockford_open",
+				"gm_atomic",
+				"rp_bmrf",
+				"rp_city17_build210",
+				"rp_ineu_valley2_v1a",
+				"rp_sectorc_beta",
+				"rp_southside"
+			};
+			string Gamemode = "sandbox"; // Name of the gamemode folder
+			int MaxPlayers = 24; // Number of players allowed on the server
+			// End Config
 
-            Random randstr = new Random();
-            int randmap = randstr.Next( 0, Maps.Length );
+			Random randstr = new Random();
+			int randmap = randstr.Next( 0, Maps.Length );
+			string Token = System.IO.File.ReadAllText( Dir + @"\token.txt" );
 
-            var proc = new ProcessStartInfo
-            {
-                UseShellExecute = true,
-                WorkingDirectory = Dir,
-                FileName = Dir + @"\srcds.exe",
-                Arguments = "+gamemode " + Gamemode + " -console +map " + Maps[randmap] + " +maxplayers " + MaxPlayers + " +r_hunkalloclightmaps 0",
-                WindowStyle = ProcessWindowStyle.Maximized
-            };
+			var proc = new ProcessStartInfo
+			{
+				UseShellExecute = true,
+				WorkingDirectory = Dir,
+				FileName = Dir + @"\srcds.exe",
+				Arguments = "+gamemode " + Gamemode + " -console +map " + Maps[randmap] + " +maxplayers " + MaxPlayers + " +r_hunkalloclightmaps 0 +sv_setsteamaccount " + Token,
+				WindowStyle = ProcessWindowStyle.Maximized
+			};
 
-            try
-            {
-                Process.Start( proc );
-            }
-            catch( Exception )
-            {
-                Console.WriteLine( "Failed to launch. Make sure the file path is correct." );
-                Console.ReadLine();
-                Environment.Exit( 3 );
-            }
-        }
-    }
+			try
+			{
+				Process.Start( proc );
+			}
+			catch ( Exception )
+			{
+				Console.WriteLine( "Failed to launch. Make sure the file path is correct." );
+				Console.ReadLine();
+				Environment.Exit( 3 );
+			}
+		}
+	}
 }
